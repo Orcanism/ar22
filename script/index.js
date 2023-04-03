9726d8e28255c2f2b4b24d48cb58a660ca50bd5f
Orcanism <louis.orca.brunner@gmail.com>
2023-04-03 21:25:43 +0200

    nothing interesting here

--- script/index.js
+++ script/index.js
@@ -5,11 +5,17 @@ function dropdownMenu() {
     if (isOpen) {
         document.getElementsByClassName('dropdownMenu')[0].style.opacity = 0;
         document.getElementsByClassName('header')[0].style.backgroundColor = 'transparent';
+        for (let i = 0; i < 5; i++) {
+            document.getElementsByClassName('dropdownMenu')[0].children[i].style.pointerEvents = 'none'
+        }
         isOpen = false;
     }
     else {
         document.getElementsByClassName('dropdownMenu')[0].style.opacity = 1;
         document.getElementsByClassName('header')[0].style.backgroundColor = 'lightgrey';
+        for (let i = 0; i < 5; i++) {
+            document.getElementsByClassName('dropdownMenu')[0].children[i].style.pointerEvents = 'all'
+        }
         isOpen = true;
     }
 }

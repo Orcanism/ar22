f68d60dfcf7e6c2bf244aad1da8559745c4dcb37
PalexSlyth <cyril.simon67@gmail.com>
2023-04-04 12:11:10 +0200

    test formu + tri

--- php/process-form.php
+++ php/process-form.php
@@ -6,7 +6,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $subject = $_POST["subject"];
   $message = $_POST["message"];
 
-  // effectue les actions appropriées, comme envoyer un e-mail ou enregistrer les données dans une base de données
+  $to = "cyrilsimonodu67@gmail.com";
+  $from = "expediteur@example.com";
+  $headers = "From: $from\r\nReply-To: $email\r\n";
+  $body = "Nom: $name\nE-mail: $email\nSujet: $subject\nMessage:\n$message";
+  mail($to, $subject, $body, $headers);
 
   // envoie une réponse au client
   echo "Merci pour votre message, $name. Nous vous contacterons bientôt à l'adresse $email.";
